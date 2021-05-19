7172888d1c2e0378c0b948b78971d0b6f8ec9613
Jim Vincent <e0425954@u.nus.edu>
2021-05-19 21:20:00 +0800

    Completed Card and CardList

---
+++ src/Card.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+
+
+// const Card = (props) => {
+//     return (
+//         <div className='bg-light-green dib hr3 pa3 ma2 grow bw2 shadow-5'>
+//             <img alt='robot' src={`https://robohash.org/${props.id}?200x200`}/>
+//             <div>
+//                 <h2>{props.name}</h2>
+//                 <p>{props.email}</p>
+//             </div>
+//         </div>
+//     );
+// }
+// export default Card;
+
+
+// or
+
+
+const Card = ({ id, name, email}) => {
+    return (
+        <Fragment>
+            <div className='tc bg-light-green dib hr3 pa3 ma2 grow bw2 shadow-5'>
+                <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// ./ means it is in the same directory
+// import App from './App';
+// if you never put .something, it assumes that it is JS
+// import reportWebVitals from './reportWebVitals';
+// import Hello from './Hello';
+// import Card from './Card';
+import CardList from './CardList';
+import 'tachyons';
+import { robots } from './robots';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<CardList robots={robots} />,
   document.getElementById('root')
 );
+
+// or 
+// using fragments
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
