4b2917a32bda207c7b58ef8ce732b49b81082681
Jim Vincent <e0425954@u.nus.edu>
2021-05-27 23:58:40 +0800

    Using React Hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import CardList from '../containers/CardList';
 import SearchBox from '../containers/SearchBox';
 import './App.css';
@@ -8,59 +8,73 @@ import ErrorBoundary from './ErrorBoundary.js';
 // STATE >> props
 
 
-class App extends Component {
-    constructor() {
-        super();
-        // something that can change, lives in the parent
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App () {
+    // constructor() {
+    //     super();
+    //     // something that can change, lives in the parent
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+
+    const [robots, setRobots] = useState([]);
+    // [] is the initial state of robots
+    const [searchfield, setSearchfield] = useState('');
 
     // life cycle function / in built function so do not need arrow keys
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users').then(response=> {
+    //         return response.json();
+    //     })
+    //     .then(users => {this.setState({ robots: users})});
+    //     // don't need to use {} if one return statement.
+    //     // need to use {} and return statement if multiple lines
+    // }
+
+    // useeffect for side effects
+    // gets run every time the function App is being run
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users').then(response=> {
             return response.json();
         })
-        .then(users => {this.setState({ robots: users})});
-        // don't need to use {} if one return statement.
-        // need to use {} and return statement if multiple lines
-    }
+        .then(users => setRobots(users));
+    }, [searchfield]) //only run if searchfield change
+    // THE SECOND PARAMETER DETERMINES WHEN THE USEEFFECT RENDERS
+    // if search field change then re render,
+    // or 
+// }, []) ==> this array will never change, so same effect as oncomponentdidmount!
 
     // custom function, so need to use arrows functions
-    onSearchChange = (event) => {
+    const onSearchChange = (event) => {
         // setState is a standard synthax
-        this.setState({ searchfield: event.target.value})
-        console.log(event.target.value);
+        setSearchfield(event.target.value)
     }
-    render() {
-        const { robots, searchfield} = this.state;
-        
-        const filteredRobots = robots.filter(
-            robot => {
-                return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-            }
-        );
 
-        if (!robots.length) {
-            return <h1>Loading...</h1>
-        } else {
-            return (
-                <>
-                    <div className='tc'>
-                        <h1 className='f1'>RoboFriends</h1>
-                        <SearchBox searchChange={this.onSearchChange}/>
-                        <Scroll>
-                            <ErrorBoundary>
-                                <CardList robots={filteredRobots}/>
-                            </ErrorBoundary>
-                        </Scroll>
-                    </div>
-                </>
-            );
+    const filteredRobots = robots.filter(
+        robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         }
+    );
+
+    if (!robots.length) {
+        return <h1>Loading...</h1>
+    } else {
+        return (
+            <>
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            </>
+        );
     }
 }
+
 
 export default App
