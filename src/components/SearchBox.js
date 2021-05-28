9432c14e4f1a510cc35d55a212a5d20dc6e44590
Jim Vincent <e0425954@u.nus.edu>
2021-05-29 02:03:14 +0800

    minor change

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,7 +6,7 @@ const SearchBox = ({searchChange}) => {
             <input 
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search' 
-                placeholder='search robots'
+                placeholder='Search Friends'
                 onChange={searchChange}
             />
             {/* on Change is a Pre-built function */}
